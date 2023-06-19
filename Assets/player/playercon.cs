0a6ed6573746bd519d70589d4b6ad77e06923f38
DESKTOP-MH3M7B4\user <nkc23.ichioka.yuta@gmail.com>
2023-06-19 12:08:32 +0900

    6/19更新

--- Assets/player/playercon.cs
+++ Assets/player/playercon.cs
@@ -2,37 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class playercon : MonoBehaviour
-{Vector3 dir = Vector3.zero;//移動方向を保存
+{
+    float speed = 20f;
+    Vector3 dir = Vector3.zero; // 移動方向を保存する変数
 
-    Animator anim;
-    // Start is called before the first frame update
+    Animator anim;// アニメーターコンポーネントの情報を保存する変数
+    public GameObject shot;
     void Start()
     {
+        // アニメーターコンポーネントの情報を保存
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float speed = 5;
+
+
+        // 移動方向をセット
         dir.x = Input.GetAxisRaw("Horizontal");
         dir.y = Input.GetAxisRaw("Vertical");
-        transform.position += dir.normalized  *  speed * Time.deltaTime;
-        //画面内移動制限
+        transform.position += dir.normalized * speed * Time.deltaTime;
+
+        // 画面内移動制限
         Vector3 pos = transform.position;
         pos.x = Mathf.Clamp(pos.x, -9f, 9f);
         pos.y = Mathf.Clamp(pos.y, -5f, 5f);
         transform.position = pos;
 
-
-        //アニメーションの設定
-        if(dir.y==0) 
+        // アニメーション設定
+        if (dir.y == 0)
         {
+            // アニメーションクリップ【Player】を再生
             anim.Play("player");
         }
-        else if (dir.y==1) 
+        else if (dir.y == 1)
         {
             anim.Play("playerL");
         }
@@ -40,7 +46,13 @@ public class playercon : MonoBehaviour
         {
             anim.Play("playerR");
         }
-
-
+        float g = 0;
+        g += Time.deltaTime;
+        if (Input.GetKey(KeyCode.Z)&& 0.8>= g)
+        {
+            GameObject go = Instantiate(shot);
+            go.transform.position = new Vector3((pos.x + 0.01f), (pos.y+1f), 0);
+            go.transform.Translate(1, 0, 0);
+        }
     }
 }
